029ad18d1554071114a29c05448224b7f24db1c2
Hanno Fellmann <hanno@fellmannweb.de>
2019-12-31 09:58:10 +0100

    remove codemirror

--- index.jsx
+++ index.jsx
@@ -1,31 +1,38 @@
 import React from "react";
-import { UnControlled as CodeMirror } from "react-codemirror2";
 import { DndProvider, useDrop } from "react-dnd";
 import Backend from "react-dnd-html5-backend";
 import ReactDOM from "react-dom";
-
-import "codemirror/lib/codemirror.css";
 
 const DropTarget = props => {
   const [collectedProps, drop] = useDrop({
     accept: "test"
   });
 
-  return <div ref={drop}>Drop Target</div>;
+  return <div ref={drop}>DndProvider -> useDrop</div>;
 };
-
 class Test extends React.Component {
+  constructor() {
+    super();
+    this.state = { showTest: true };
+  }
+
   render() {
     return (
       <div style={{ backgroundColor: "#ccc" }}>
-        <div style={{ border: "1px solid black" }}>
-          <CodeMirror value="Try to drag some text" />
-        </div>
-        <div style={{ border: "1px solid black" }}>
-          <DndProvider backend={Backend}>
-            <DropTarget />
-          </DndProvider>
-        </div>
+        <input
+          id="check"
+          type="checkbox"
+          checked={this.state.showTest}
+          onChange={() => this.setState({ showTest: !this.state.showTest })}
+        />{" "}
+        <label htmlFor="check"> Enable React Dnd</label>
+        {this.state.showTest && (
+          <div style={{ border: "1px solid black" }}>
+            <DndProvider backend={Backend}>
+              <DropTarget />
+            </DndProvider>
+          </div>
+        )}
       </div>
     );
   }
